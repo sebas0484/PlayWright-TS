5091d6c5d5058dccd8f4ed2d32f5e3328acd684f
sebastian <semuron.2@gmail.com>
2024-12-12 07:07:49 -0500

    pregunta que el actor hace para recuperar los datos que fueron introducidos en el formulario, como el nombre, correo electrónico, teléfono, etc.

---
+++ tests/src/questions/FormData.ts
@@ -0,0 +1,27 @@
+// src/questions/FormData.ts
+import { Page } from '@playwright/test';
+
+interface FormData {
+  nameField: string;
+  emailField: string;
+  gender: string;
+  mobile: string;
+  dob: string;
+  stateCity: string;
+}
+
+class FormDataQuestion {
+    static async answer(page: Page): Promise<FormData> {
+      const nameField = await page.locator('tbody > tr:nth-child(1) > td:nth-child(2)').textContent() || '';
+      const emailField = await page.locator('tbody > tr:nth-child(2) > td:nth-child(2)').textContent() || '';
+      const gender = await page.locator('tbody > tr:nth-child(3) > td:nth-child(2)').textContent() || '';
+      const mobile = await page.locator('tbody > tr:nth-child(4) > td:nth-child(2)').textContent() || '';
+      const dob = await page.locator('tbody > tr:nth-child(5) > td:nth-child(2)').textContent() || '';
+      const stateCity = await page.locator('tbody > tr:nth-child(10) > td:nth-child(2)').textContent() || '';
+  
+      return { nameField, emailField, gender, mobile, dob, stateCity };
+    }
+  }
+  
+
+export default FormDataQuestion;
